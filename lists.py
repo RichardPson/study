b945db3d4806d7a3af3c90fc34bc3fa05ee0a821
RichardPson <rip.richard@gmail.com>
2021-01-31 22:16:46 +0100

    s1

--- lists.py
+++ lists.py
@@ -1,5 +1,11 @@
 # sorting lists etc
+# return a list that only contain the common elelemts between list a and b. 
 
 a_list = [1, 3, 5, 7, 9]
 b_list = [2, 3, 4, 5, 6, 7, 8]
+c_list = []
+
+for i in a_list:
+    print(a_list)
+
 
